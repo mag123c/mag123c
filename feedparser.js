678891f148ee379692e4113b673304154d08f62e
mag123c <diehreo@gmail.com>
2024-05-26 13:15:35 +0900

    Update feedparser.js

--- feedparser.js
+++ feedparser.js
@@ -9,38 +9,46 @@ const parser = new Parser({
     }});
 
 (async () => {
+    try {
+        const feed = await parser.parseURL('https://mag1c.tistory.com/rss');
+        console.log('RSS feed fetched successfully.');
+        
+        for (const feedItem of feed.items) {
+            const {title, link} = feedItem;
+            text += `- [${title}](${link})</br>\n`;
+        }
 
-    const feed = await parser.parseURL('https://mag1c.tistory.com/rss');
-    for (const feedItem of feed.items) {
-        const {title, link} = feedItem;
-        text += `- [${title}](${link})</br>\n`;
-    }
-
-    let readmeContent = readFileSync('README.md', 'utf8');
-
-    const startMarker = '## 📕 Recent Posting';
-    const endMarker = '\n## ';
-    const startIndex = readmeContent.indexOf(startMarker);
-    const endIndex = readmeContent.indexOf(endMarker, startIndex + startMarker.length);
-    
-    if (startIndex !== -1 && endIndex !== -1) {
-        const before = readmeContent.substring(0, startIndex);
-        const after = readmeContent.substring(endIndex);
-        readmeContent = `${before}${text}${after}`;
-    } else if (startIndex !== -1) {
-        readmeContent = readmeContent.substring(0, startIndex) + text;
-    } else {
-        readmeContent += `\n${text}`;
-    }
-
-    if (readFileSync('README.md', 'utf8') !== readmeContent) {
-        writeFileSync('README.md', readmeContent, 'utf8', (e) => {
-            if (e) {
-                console.log(e);
-            }
-        });
+        console.log('Parsed RSS feed items:');
         console.log(text);
-    } else {
-        console.log('No changes detected in README.md');
+
+        let readmeContent = readFileSync('README.md', 'utf8');
+        console.log('Current README.md content fetched.');
+
+        const startMarker = '## 📕 Recent Posting';
+        const endMarker = '\n## ';
+        const startIndex = readmeContent.indexOf(startMarker);
+        const endIndex = readmeContent.indexOf(endMarker, startIndex + startMarker.length);
+
+        if (startIndex !== -1 && endIndex !== -1) {
+            const before = readmeContent.substring(0, startIndex);
+            const after = readmeContent.substring(endIndex);
+            readmeContent = `${before}${text}${after}`;
+        } else if (startIndex !== -1) {
+            readmeContent = readmeContent.substring(0, startIndex) + text;
+        } else {
+            readmeContent += `\n${text}`;
+        }
+
+        console.log('Updated README.md content:');
+        console.log(readmeContent);
+
+        if (readFileSync('README.md', 'utf8') !== readmeContent) {
+            writeFileSync('README.md', readmeContent, 'utf8');
+            console.log('README.md has been updated and saved.');
+        } else {
+            console.log('No changes detected in README.md');
+        }
+    } catch (error) {
+        console.error('Error updating README.md:', error);
     }
 })();
